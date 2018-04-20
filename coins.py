856f6ef5d73a72ff847a2264ec470992b3eb4d14
0ooo00ooo0 <lavegute@gmail.com>
2018-04-20 03:13:34 +0200

    condition btc

--- coins.py
+++ coins.py
@@ -18,6 +18,7 @@ from colorclass import Color
 
 
 ####################################################################################
+
 api_name = "https://api.coinmarketcap.com/v1/ticker/"
 name_json = json.load(urllib2.urlopen(api_name))
 
@@ -31,12 +32,11 @@ print "\n\n\n\n"
 
 ####################################################################################
 
-
 # INPUT STRING
 coin = raw_input("Name coin is : ")
 nb_coins_string =  raw_input("How many coins : ")
-question_btc_coin = raw_input("Do you still have bitcoins? (yes or no) : ")
-
+# CONVERSION STRING  INPUT EN FLOAT
+nb_coins = float(nb_coins_string)
 
 # SI VIDE
 if not nb_coins_string :
@@ -47,16 +47,271 @@ if not nb_coins_string :
 	print Color("{autored}Put your coins amount or put 0{/autored}")
 	print "\n\n"
 
- 
-# CONVERSION STRING  INPUT EN FLOAT
-nb_coins = float(nb_coins_string)
-
-
-if question_btc_coin == "yes" :
+####################################################################################
 	
-	# SI IL RESTE DES BITCOINS, COMBIEN EN A T ON ?
-	question_btc_coin = raw_input("How many bitcoins do you have left ? : ")
-	potentiel_buy = float(question_btc_coin)
+		
+		
+if coin != "bitcoin" :
+
+	question_btc_coin = raw_input("Do you still have bitcoins? (yes or no) : ")
+	
+	if question_btc_coin == "yes" :
+		
+		# SI IL RESTE DES BITCOINS, COMBIEN EN A T ON ?
+		question_btc_coin = raw_input("How many bitcoins do you have left ? : ")
+		potentiel_buy = float(question_btc_coin)
+
+		while True :
+
+			# JSON coin
+			api_coin = "https://api.coinmarketcap.com/v1/ticker/" + coin + "/?convert=EUR"
+			coin_json = json.load(urllib2.urlopen(api_coin))
+
+			# JSON BTC
+			api_btc  = "https://api.coinmarketcap.com/v1/ticker/bitcoin/?convert=EUR";
+			btc_json = json.load(urllib2.urlopen(api_btc))
+			
+
+			# VALEUR D'UN BITCOIN E EURO
+			cours_btc = float(btc_json[0]["price_eur"])
+
+
+			# VALEUR DE LA MONNAIE EN EURO, USD, BTC
+			coin_euro = round(float(coin_json[0]["price_eur"]),2)
+			coin_usd  = round(float(coin_json[0]["price_usd"]),2)
+			coin_btc  = float(coin_json[0]["price_btc"])
+
+			# POURCENTAGE
+			btc_percent = str(btc_json[0]["percent_change_24h"])
+			coin_percent = str(coin_json[0]["percent_change_24h"])
+			# EXPLODE DE LA CHAINE POUR RECUPERER "-"
+			split_coins = coin_percent.split('.')
+			split_btc = btc_percent.split('.')
+			
+			
+			# SOMME DE COINS EN BITCOIN
+			total_coin_btc = float(nb_coins * coin_btc)
+			# PRODUIT EN X, TOTAL DE COIN EN BTC * PRIX DU BTC EN EURO / 1
+			total_coin_eur = round(float(total_coin_btc * cours_btc),2)
+
+
+
+
+			#------------------COURS BTC---------------------#
+
+			if coin != "bitcoin" :
+				
+				from terminaltables import AsciiTable
+				
+				if split_btc[0][0] == "-" :
+				
+					table_data = [
+						['1 BTC --> Euro','BTC %'],
+						[Color('{autored}'+ str(cours_btc) + '{/autored}'),
+						 Color('{autored}'+ str(btc_percent) + '{/autored}')]
+					]
+					table = AsciiTable(table_data)
+					print table.table
+					
+				else :
+				
+					table_data = [
+						['1 BTC --> Euro','BTC %'],
+						[Color('{autored}'+ str(cours_btc) + '{/autored}'),
+						 Color('{autogreen}'+ str(btc_percent) + '{/autogreen}')]
+					]
+					table = AsciiTable(table_data)
+					print table.table
+					
+			else : 
+				
+				table_data = [
+					['1 BTC --> Euro'],
+					[Color('{autored}'+ str(cours_btc) + '{/autored}')]
+				]
+				table = AsciiTable(table_data)
+				print table.table
+				
+			#------------------POTENTIEL---------------------#
+			
+			from terminaltables import AsciiTable
+			table_data = [
+				[ 'Achat potentiel', 'Total potentiel'],
+				[Color('{autored}'+ str(potentiel_buy / coin_btc) + '{/autored}'),
+				Color('{autored}'+ str(potentiel_buy / coin_btc + nb_coins) + '{/autored}')]
+			]
+			table = AsciiTable(table_data)
+			print table.table
+			
+			#-----------------SOMME TOTAL--------------------#
+			
+			from terminaltables import AsciiTable
+			
+			if split_coins[0][0] == "-" :
+		
+				table_data = [
+					['TOTAL ' + coin +'','TOTAL BTC', 'TOTAL euros', coin +' %'],
+					[Color('{autored}' + str(nb_coins) + '{/autored}'),
+					 Color('{autored}' + "{:.8f}".format(total_coin_btc) + '{/autored}'),
+					 Color('{autored}' + str(total_coin_eur) + '{/autored}'),
+					 Color('{autored}' + str(coin_percent) + '{/autored}')]				   
+				]
+				table = AsciiTable(table_data)
+				print table.table
+			
+			else : 
+		
+				table_data = [
+					['TOTAL ' + coin +'','TOTAL BTC', 'TOTAL euros', coin +' %'],
+					[Color('{autored}' + str(nb_coins) + '{/autored}'),
+					 Color('{autored}' + "{:.8f}".format(total_coin_btc) + '{/autored}'),
+					 Color('{autored}' + str(total_coin_eur) + '{/autored}'),
+					 Color('{autogreen}' + str(coin_percent) + '{/autogreen}')]				   
+				]
+				table = AsciiTable(table_data)
+				print table.table
+
+			#------------------COURS COINS---------------------#
+
+			from terminaltables import AsciiTable
+			table_data = [
+				['Cours '+ coin +' --> $', 'Cours ' + coin + ' --> EURO', 'Cours ' + coin +' --> SATOSHIS'],
+				[Color('{autored}' + str(coin_usd) + '{/autored}'), 
+				 Color('{autored}' + str(coin_euro) + '{/autored}'),
+				 Color('{autored}' + "{:.8f}".format(coin_btc) + '{/autored}')]
+			]
+			table = AsciiTable(table_data)
+			print table.table
+
+			# ACTUALISATION API 
+			time.sleep(5)
+			# CMD CLEAR
+			os.system('cls' if os.name == 'nt' else 'clear')
+			
+	else  :
+		
+		while True :
+
+			# JSON coin
+			api_coin = "https://api.coinmarketcap.com/v1/ticker/" + coin + "/?convert=EUR"
+			coin_json = json.load(urllib2.urlopen(api_coin))
+
+			# JSON BTC
+			api_btc  = "https://api.coinmarketcap.com/v1/ticker/bitcoin/?convert=EUR";
+			btc_json = json.load(urllib2.urlopen(api_btc))
+			
+
+			# VALEUR D'UN BITCOIN E EURO
+			cours_btc = float(btc_json[0]["price_eur"])
+
+
+			# VALEUR DE LA MONNAIE EN EURO, USD, BTC
+			coin_euro = round(float(coin_json[0]["price_eur"]),2)
+			coin_usd  = round(float(coin_json[0]["price_usd"]),2)
+			coin_btc  = float(coin_json[0]["price_btc"])
+
+			# POURCENTAGE
+			btc_percent = str(btc_json[0]["percent_change_24h"])
+			coin_percent = str(coin_json[0]["percent_change_24h"])
+			# EXPLODE DE LA CHAINE POUR RECUPERER "-"
+			split_coins = coin_percent.split('.')
+			split_btc = btc_percent.split('.')
+			
+			
+			# SOMME DE COINS EN BITCOIN
+			total_coin_btc = float(nb_coins * coin_btc)
+			# PRODUIT EN X, TOTAL DE COIN EN BTC * PRIX DU BTC EN EURO / 1
+			total_coin_eur = round(float(total_coin_btc * cours_btc),2)
+
+
+			
+			
+			#------------------COURS BTC---------------------#
+							
+			if coin != "bitcoin" :
+			
+				from terminaltables import AsciiTable
+				
+				if split_btc[0][0] == "-" :
+				
+					table_data = [
+						['1 BTC --> Euro','BTC %'],
+						[Color('{autored}'+ str(cours_btc) + '{/autored}'),
+						 Color('{autored}'+ str(btc_percent) + '{/autored}')]
+					]
+					table = AsciiTable(table_data)
+					print table.table
+					
+				else :
+				
+					table_data = [
+						['1 BTC --> Euro','BTC %'],
+						[Color('{autored}'+ str(cours_btc) + '{/autored}'),
+						 Color('{autogreen}'+ str(btc_percent) + '{/autogreen}')]
+					]
+					table = AsciiTable(table_data)
+					print table.table
+					
+			else : 
+				
+				table_data = [
+					['1 BTC --> Euro'],
+					[Color('{autored}'+ str(cours_btc) + '{/autored}')]
+				]
+				table = AsciiTable(table_data)
+				print table.table
+
+			#-----------------SOMME TOTAL--------------------#
+			
+			from terminaltables import AsciiTable
+			
+			if split_coins[0][0] == "-" :
+			
+				table_data = [
+					['TOTAL ' + coin +'','TOTAL BTC', 'TOTAL euros', coin +' %'],
+					[Color('{autored}' + str(nb_coins) + '{/autored}'),
+					 Color('{autored}' + "{:.8f}".format(total_coin_btc) + '{/autored}'),
+					 Color('{autored}' + str(total_coin_eur) + '{/autored}'),
+					 Color('{autored}' + str(coin_percent) + '{/autored}')]				   
+				]
+				table = AsciiTable(table_data)
+				print table.table
+				
+			else : 
+			
+				from terminaltables import AsciiTable
+				table_data = [
+					['TOTAL ' + coin +'','TOTAL BTC', 'TOTAL euros', coin +' %'],
+					[Color('{autored}' + str(nb_coins) + '{/autored}'),
+					 Color('{autored}' + "{:.8f}".format(total_coin_btc) + '{/autored}'),
+					 Color('{autored}' + str(total_coin_eur) + '{/autored}'),
+					 Color('{autogreen}' + str(coin_percent) + '{/autogreen}')]				   
+				]
+				table = AsciiTable(table_data)
+				print table.table
+
+			#------------------COURS COINS---------------------#
+
+			from terminaltables import AsciiTable
+			table_data = [
+				['Cours '+ coin +' --> $', 'Cours ' + coin + ' --> EURO', 'Cours ' + coin +' --> SATOSHIS'],
+				[Color('{autored}' + str(coin_usd) + '{/autored}'), 
+				 Color('{autored}' + str(coin_euro) + '{/autored}'),
+				 Color('{autored}' + "{:.8f}".format(coin_btc) + '{/autored}')]
+			]
+			table = AsciiTable(table_data)
+			print table.table
+
+			# ACTUALISATION API 
+			time.sleep(5)
+			# CMD CLEAR
+			os.system('cls' if os.name == 'nt' else 'clear')
+			
+
+###################################################################"
+	
+	
+elif coin == "bitcoin" :
 
 	while True :
 
@@ -79,11 +334,9 @@ if question_btc_coin == "yes" :
 		coin_btc  = float(coin_json[0]["price_btc"])
 
 		# POURCENTAGE
-		btc_percent = str(btc_json[0]["percent_change_24h"])
 		coin_percent = str(coin_json[0]["percent_change_24h"])
 		# EXPLODE DE LA CHAINE POUR RECUPERER "-"
 		split_coins = coin_percent.split('.')
-		split_btc = btc_percent.split('.')
 		
 		
 		# SOMME DE COINS EN BITCOIN
@@ -95,146 +348,15 @@ if question_btc_coin == "yes" :
 
 
 		#------------------COURS BTC---------------------#
-
-		from terminaltables import AsciiTable
-		# SI "-" EXISTE, POURENTAGE AFFICHE EN ROUGE
-		if split_btc[0][0] == "-" :
-		
-			table_data = [
-				['1 BTC --> Euro','BTC %'],
-				[Color('{autored}'+ str(cours_btc) + '{/autored}'),
-				 Color('{autored}'+ str(btc_percent) + '{/autored}')]
-			]
-			table = AsciiTable(table_data)
-			print table.table
-			
-		else :
-		
-			table_data = [
-				['1 BTC --> Euro','BTC %'],
-				[Color('{autored}'+ str(cours_btc) + '{/autored}'),
-				 Color('{autogreen}'+ str(btc_percent) + '{/autogreen}')]
-			]
-			table = AsciiTable(table_data)
-			print table.table
-			
-		#------------------POTENTIEL---------------------#
 		
 		from terminaltables import AsciiTable
+		# SUPPRESSION % 
 		table_data = [
-			[ 'Achat potentiel', 'Total potentiel'],
-			[Color('{autored}'+ str(potentiel_buy / coin_btc) + '{/autored}'),
-			Color('{autored}'+ str(potentiel_buy / coin_btc + nb_coins) + '{/autored}')]
+			['1 BTC --> Euro'],
+			[Color('{autored}'+ str(cours_btc) + '{/autored}')]
 		]
 		table = AsciiTable(table_data)
 		print table.table
-		
-		#-----------------SOMME TOTAL--------------------#
-		
-		from terminaltables import AsciiTable
-		
-		if split_coins[0][0] == "-" :
-	
-			table_data = [
-				['TOTAL ' + coin +'','TOTAL BTC', 'TOTAL euros', coin +' %'],
-				[Color('{autored}' + str(nb_coins) + '{/autored}'),
-				 Color('{autored}' + "{:.8f}".format(total_coin_btc) + '{/autored}'),
-				 Color('{autored}' + str(total_coin_eur) + '{/autored}'),
-				 Color('{autored}' + str(coin_percent) + '{/autored}')]				   
-			]
-			table = AsciiTable(table_data)
-			print table.table
-		
-		else : 
-	
-			table_data = [
-				['TOTAL ' + coin +'','TOTAL BTC', 'TOTAL euros', coin +' %'],
-				[Color('{autored}' + str(nb_coins) + '{/autored}'),
-				 Color('{autored}' + "{:.8f}".format(total_coin_btc) + '{/autored}'),
-				 Color('{autored}' + str(total_coin_eur) + '{/autored}'),
-				 Color('{autogreen}' + str(coin_percent) + '{/autogreen}')]				   
-			]
-			table = AsciiTable(table_data)
-			print table.table
-
-		#------------------COURS COINS---------------------#
-
-		from terminaltables import AsciiTable
-		table_data = [
-			['Cours '+ coin +' --> $', 'Cours ' + coin + ' --> EURO', 'Cours ' + coin +' --> SATOSHIS'],
-			[Color('{autored}' + str(coin_usd) + '{/autored}'), 
-			 Color('{autored}' + str(coin_euro) + '{/autored}'),
-			 Color('{autored}' + "{:.8f}".format(coin_btc) + '{/autored}')]
-		]
-		table = AsciiTable(table_data)
-		print table.table
-
-		# ACTUALISATION API 
-		time.sleep(5)
-		# CMD CLEAR
-		os.system('cls' if os.name == 'nt' else 'clear')
-		
-else :
-	
-	while True :
-
-		# JSON coin
-		api_coin = "https://api.coinmarketcap.com/v1/ticker/" + coin + "/?convert=EUR"
-		coin_json = json.load(urllib2.urlopen(api_coin))
-
-		# JSON BTC
-		api_btc  = "https://api.coinmarketcap.com/v1/ticker/bitcoin/?convert=EUR";
-		btc_json = json.load(urllib2.urlopen(api_btc))
-		
-
-		# VALEUR D'UN BITCOIN E EURO
-		cours_btc = float(btc_json[0]["price_eur"])
-
-
-		# VALEUR DE LA MONNAIE EN EURO, USD, BTC
-		coin_euro = round(float(coin_json[0]["price_eur"]),2)
-		coin_usd  = round(float(coin_json[0]["price_usd"]),2)
-		coin_btc  = float(coin_json[0]["price_btc"])
-
-		# POURCENTAGE
-		btc_percent = str(btc_json[0]["percent_change_24h"])
-		coin_percent = str(coin_json[0]["percent_change_24h"])
-		# EXPLODE DE LA CHAINE POUR RECUPERER "-"
-		split_coins = coin_percent.split('.')
-		split_btc = btc_percent.split('.')
-		
-		
-		# SOMME DE COINS EN BITCOIN
-		total_coin_btc = float(nb_coins * coin_btc)
-		# PRODUIT EN X, TOTAL DE COIN EN BTC * PRIX DU BTC EN EURO / 1
-		total_coin_eur = round(float(total_coin_btc * cours_btc),2)
-
-
-
-
-		#------------------COURS BTC---------------------#
-
-		from terminaltables import AsciiTable
-		
-		if split_btc[0][0] == "-" :
-		
-			table_data = [
-				['1 BTC --> Euro','BTC %'],
-				[Color('{autored}'+ str(cours_btc) + '{/autored}'),
-				 Color('{autored}'+ str(btc_percent) + '{/autored}')]
-			]
-			table = AsciiTable(table_data)
-			print table.table
-			
-		else :
-		
-			table_data = [
-				['1 BTC --> Euro','BTC %'],
-				[Color('{autored}'+ str(cours_btc) + '{/autored}'),
-				 Color('{autogreen}'+ str(btc_percent) + '{/autogreen}')]
-			]
-			table = AsciiTable(table_data)
-			print table.table
 
 		#-----------------SOMME TOTAL--------------------#
 		
